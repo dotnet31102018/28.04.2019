7cf990dbc0d02b9c1f93e0221ecfd9111ed7170e
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-04-29 23:10:19 +0300

    Add files via upload

---
+++ AppConfigDemo/MyConfDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConfDemo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string site = ConfigurationManager.AppSettings["site-url"];
+
+            Console.WriteLine($"our company web site {site}");
+
+            Console.WriteLine("======================= Menu : ");
+            Console.WriteLine(ConfigurationManager.AppSettings["option1"]);
+            Console.WriteLine(ConfigurationManager.AppSettings["option2"]);
+            Console.WriteLine(ConfigurationManager.AppSettings["option3"]);
+
+            int maxAge = Convert.ToInt32(ConfigurationManager.AppSettings["maxAge"]);
+
+            int age = -1;
+            do
+            {
+                Console.WriteLine("Enter age");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            while (age <= 0 || age > maxAge);
+
+        }
+    }
+}
